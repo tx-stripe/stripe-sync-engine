6470de30e1235a71ea038345a83ac2c774c02ee1
Luis Fernando De Pombo <2381303+depombo@users.noreply.github.com>
2025-11-18 18:13:32 -0800

    fix lint

--- packages/sync-engine/src/types.ts
+++ packages/sync-engine/src/types.ts
@@ -5,9 +5,9 @@ import Stripe from 'stripe'
  * Simple logger interface compatible with both pino and console
  */
 export interface Logger {
-  info(...args: any[]): void
-  warn(...args: any[]): void
-  error(...args: any[]): void
+  info(message?: unknown, ...optionalParams: unknown[]): void
+  warn(message?: unknown, ...optionalParams: unknown[]): void
+  error(message?: unknown, ...optionalParams: unknown[]): void
 }
 
 export type RevalidateEntity =
